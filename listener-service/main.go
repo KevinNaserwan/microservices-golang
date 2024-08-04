f2114602f3647b70facad1ceaadc7ea77d06769a
kevinnaserwan <kevinnaserwan@gmail.com>
2024-08-04 22:43:44 +0700

    Feat: add listener service

---
+++ listener-service/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	"github.com/kevinnaserwan/listener-service/event"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	// try to connect to rabbitmq
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	defer rabbitConn.Close()
+
+	// start listening for message
+	log.Println("Listening for and consuming RabbitMQ messages...")
+
+	// create consumer
+	consumer, err := event.NewConsumer(rabbitConn)
+	if err != nil {
+		panic(err)
+	}
+
+	// watch the queue and consume events
+	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backoff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// don't continue until rabbit is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			log.Println("Connected to RabbitMQ!")
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backoff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+
+		log.Println("backing 0ff...")
+		time.Sleep(backoff)
+		continue
+	}
+
+	return connection, nil
+}
